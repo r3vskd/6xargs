46cd8562d094369095b6c296bd762f305ecd3f9f
r3vskd <dymethylcadmium@protonmail.ch>
2024-12-08 23:45:45 -0600

    routes & api

--- src/routes/api.js
+++ src/routes/api.js
@@ -4,23 +4,23 @@ const router = express.Router();
 
 // Ejemplo de rutas
 router.get('/status', (req, res) => {
-  res.status(200).json({ message: 'API is working fine!' });
+  res.status(200).json({ message: 'API is working fine!!' });
 });
 
 router.post('/data', (req, res) => {
   const data = req.body;
-  res.status(201).json({ message: 'Data received successfully!', data });
+  res.status(201).json({ message: 'Data received successfully!!', data });
 });
 
 router.put('/update/:id', (req, res) => {
   const { id } = req.params;
   const updatedData = req.body;
-  res.status(200).json({ message: `Data with id ${id} updated!`, updatedData });
+  res.status(200).json({ message: `Data with id ${id} updated!!`, updatedData });
 });
 
 router.delete('/delete/:id', (req, res) => {
   const { id } = req.params;
-  res.status(200).json({ message: `Data with id ${id} deleted!` });
+  res.status(200).json({ message: `Data with id ${id} deleted!!` });
 });
 
 export default router;
