9c34b30d857bebdc4d66d0529e976fe8677255bb
r3vskd <dymethylcadmium@protonmail.ch>
2024-12-13 14:31:03 -0600

    working index mods

--- src/index.js
+++ src/index.js
@@ -1,32 +1,22 @@
+// Importar las dependencias necesarias
 import express from 'express';
-import apiRoutes from './routes/api.js';
 import path from 'path';
-import { fileURLToPath } from 'url';
-import.meta.url
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const __filename = fileURLToPath(import.meta.url);
-//const __dirname = path.dirname(__filename);
+
+// Configuración de la ruta absoluta
 const __dirname = path.resolve();
 
-// Middlewares
-//app.use(express.json()); // Para parsear JSON en el cuerpo de las solicitudes
-//app.use('/assets', express.static('assets'));
-//app.use(express.static(path.join(path.resolve(), 'assets/css/styles.css')));
-//app.use(express.static(path.join(__dirname, 'assets/css')));
-app.use('/assets', express.static(path.join(__dirname, 'src')));
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-app.use(express.static(path.join(__dirname, 'assets/html')));
+// Servir archivos estáticos de la carpeta "assets"
+app.use('/assets', express.static(path.join(__dirname, 'src/assets')));
 
-app.use('/api', apiRoutes);
-
+// Ruta para servir el archivo index.html desde la carpeta 'html'
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'assets/html/index.html'));
-  res.sendFile(path.join(__dirname, 'html', 'index.html'));
-  res.sendFile(path.join(__dirname, 'css', 'styles.css'));
+  res.sendFile(path.join(__dirname, 'src/assets/html/index.html'));
 });
 
+// Iniciar el servidor
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
