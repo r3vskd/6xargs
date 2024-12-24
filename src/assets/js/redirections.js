701a0165644f91fa68dd35c59f8ceff277e4b771
r3vskd <dymethylcadmium@protonmail.ch>
2024-12-24 13:20:32 -0600

    adding redirections

--- src/assets/js/redirections.js
+++ src/assets/js/redirections.js
@@ -1,12 +1,14 @@
 function redirections(pagina) {
   const paginas = {
     login: "/assets/html/login.html",
-    dashboard
+    contact: "/assets/html/contact.html",
+    dashboard: "/assets/html/dashboard.html",
+    profile: "/assets/html/profile.html",
   };
   
-  if (paginas[pagina]) {
+if (paginas[pagina]) {
   window.location.href = paginas[pagina]; // Redirige a la página especificada
-    } else {
-        console.error("La página no existe");
-        } 
+} else {
+    console.error("La página no existe");
+ } 
 }
