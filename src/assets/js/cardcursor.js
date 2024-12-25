22b14ea7c0e855b6e183b19b9060bf3ba32c51a2
r3vskd <dymethylcadmium@protonmail.ch>
2024-12-24 18:52:07 -0600

    cards mods

--- src/assets/js/cardcursor.js
+++ src/assets/js/cardcursor.js
@@ -1,12 +1,11 @@
+// Efecto de luz que sigue al cursor en las tarjetas
 document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('mousemove', e => {
         const rect = card.getBoundingClientRect();
         const x = e.clientX - rect.left; // Coordenada X dentro de la tarjeta
         const y = e.clientY - rect.top;  // Coordenada Y dentro de la tarjeta
 
-        card.style.setProperty('--x', `${x}px`);
-        card.style.setProperty('--y', `${y}px`);
-        card.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(161, 126, 218, 0.5), transparent 70%)`;
+        card.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(255, 0, 0, 0.5), transparent 70%)`;
     });
 
     card.addEventListener('mouseleave', () => {
