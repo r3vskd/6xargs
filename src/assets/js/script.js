2a44988c0aa3366e295cda37f7dc7e3da807143b
r3vskd <dymethylcadmium@protonmail.ch>
2025-04-29 14:14:20 -0600

    redirections using javascrtip

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -12,3 +12,44 @@ document.addEventListener('DOMContentLoaded', function() {
         link.setAttribute('data-text', link.textContent);
     });
 });
+
+function goToIndex() {
+    window.location.href = '/assets/html/index.html';
+}
+
+function goToAbout() {
+    window.location.href = '/assets/html/about.html';
+}
+
+function goToProjects() {
+    window.location.href = '/assets/html/projects.html';
+}
+
+function goToContact() {
+    window.location.href = '/assets/html/contact.html';
+}
+
+function goToBlog() {
+    window.location.href = '/assets/html/blog.html';
+}
+
+function goToResume() {
+    window.location.href = '/assets/html/resume.html';
+}
+
+function goToHome() {
+    window.location.href = '/assets/html/home.html';
+}
+
+function goToServices() {
+    window.location.href = '/assets/html/services.html';
+}
+
+function goToTestimonials() {
+    window.location.href = '/assets/html/testimonials.html';
+}   
+
+function goToPortfolio() {
+    window.location.href = '/assets/html/portfolio.html';
+}
+
