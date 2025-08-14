14a1affe4ad7cb9bfd5da542522bb7cec817a4ee
r3vskd <dymethylcadmium@protonmail.ch>
2025-08-13 19:29:46 -0600

    route correction

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
 app.use('/assets', express.static(path.join(__dirname, 'src/assets')));
-app.use('/app', express.static(path.join(__dirname, 'public/build')));
+app.use('/app', express.static(path.join(__dirname, 'app/routes/')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/assets/html/index.html'));
