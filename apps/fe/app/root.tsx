4723f2516b109ec4fa4df5e05a92d859068a1f92
r3vskd <dymethylcadmium@protonmail.ch>
2025-09-17 23:37:11 -0600

    front remix starting

--- apps/fe/app/root.tsx
+++ apps/fe/app/root.tsx
@@ -9,7 +9,7 @@ import {
 import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/server-runtime";
 import { json } from "@remix-run/server-runtime";
 
-import "./tailwind.css";
+import globalStylesUrl from "./global.css?url";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -21,6 +21,10 @@ export const links: LinksFunction = () => [
   {
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+  },
+  {
+    rel: "stylesheet",
+    href: globalStylesUrl,
   },
 ];
 
